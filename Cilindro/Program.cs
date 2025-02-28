f5bbb099e32ab3b6c608ecead8630e5e8d082078
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-28 13:42:50 +0000

    calculate volume

---
+++ Cilindro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Altura: ");
+            double H = double.Parse(Console.ReadLine());
+            Console.Write("Raio: ");
+            double R = double.Parse(Console.ReadLine());
+            double V = double.Parse(Math.PI * R^2 * H);
+            
+        }
+    }
+}
