3bd4bcf51942b63cc01da1b4d7317439c1881bb6
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-28 14:40:30 +0000

    start

---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+
+            int start = 0;
+        
+            while (start < 1)
+                {
+
+                Console.Write("Start number:");
+                int start2 = int.Parse(Console.ReadLine());
+                if (start2 <1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again");
+                }
+                else
+                {
+
+                }
+                }
+
+              
+            
+
+        }
+    }
+}
