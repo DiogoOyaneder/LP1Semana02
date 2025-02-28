a35d17e2e62868c4c9117fc7e5479a332ba3e171
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-02-28 13:58:50 +0000

    add double and int

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double d1 = double.MaxValue;
+            double d2 = double.MinValue;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            int i1 = int.MaxValue;
+            int i2 = int.MinValue;
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
+        }
+    }
+}
